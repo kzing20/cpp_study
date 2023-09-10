5f4a9f654900af6e4414dd667f0208898a4ddd0d
jykim0925 <mslkd0925@gmail.com>
2023-09-10 19:39:09 +0900

    call by value, call by reference

---
+++ C++/Project3/call_by_reference.cpp
@@ -0,0 +1,18 @@
+//call by value 예
+
+#include <stdio.h>
+int sum(int* x, int* y);
+int main(void) {
+	int a = 2, b = 5, c = 0;
+	printf("sum() 호출 전 a=%d b=%d c=%d\n", a, b, c);
+	c = sum(&a, &b);
+	printf("sum() 호출 후 a=%d b=%d c=%d\n", a, b, c);
+	return 0;
+}
+int sum(int* a, int* b) {
+
+	*a = *a + 2;
+	*b = *b + 5;
+
+	return(*a + *b);
+}
